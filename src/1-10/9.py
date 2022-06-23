db97d973c5b492d4d44c0d681312893c4a1ee663
Adrian Murphy <adrian.murphy99@mail.com>
2022-06-23 22:18:42 +0200

    improved 9

--- src/1-10/9.py
+++ src/1-10/9.py
@@ -4,9 +4,9 @@ import math
 
 def find_nums():
     for i in range(1, 1000):
-        for j in range(1, 1000):
+        for j in range(i, 1000):
             a, b, c = i, j, 1000-i-j
-            if a < b < c and a**2 + b**2 == c**2:
+            if a**2 + b**2 == c**2:
                 return a, b, c
 
 print(math.prod(find_nums()))
