51d39c132cd27e43391bbf81bc046cb15b8b12de
Adrian Murphy <adrian.murphy99@mail.com>
2022-06-23 21:29:43 +0200

    solved 7

---
+++ src/1-10/7.py
@@ -0,0 +1,21 @@
+import math
+
+def eratosthenes(n):
+    primes = []
+    xs = set(x for x in range(2, n+1))
+
+    while len(xs) > 0:
+        p = xs.pop()
+        primes.append(p)
+        for i in range(p*2, n+1, p):
+            if i in xs:
+                xs.remove(i)
+
+    return primes
+
+# ~n/log(n) primes smaller than n
+n = 100
+while n / math.log(n) < 10001:
+    n += 100
+
+print(eratosthenes(n)[10000])
